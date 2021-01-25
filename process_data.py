e46ba7ba34f37834c2e2e92579970f4e3b5e77f3
Pedro Frota <pvfrota@gmail.com>
2021-01-25 18:50:05 -0400

    db: Add statistics

--- process_data.py
+++ process_data.py
@@ -2,22 +2,122 @@
 
 import pandas as pd
 
+MANAUS_ESTIMATED_POPULATION = 2219580
+
 input = "db/007_Vacinados_2021_01_23_17_17_00.json"
 output_path = 'analytics'
 
 df = pd.read_json(input)
 
-queries = {
-    'vaccination_site_count': df[['vaccination_site', 'id']].groupby('vaccination_site').count().rename(columns={'id': 'count'}).sort_values(['count'], ascending=False),
-    'area_count': df[['area', 'id']].groupby('area').count().rename(columns={'id': 'count'}).sort_values(['count'], ascending=False),
-    'cpf_count': df[['cpf', 'id']].groupby('cpf').count().rename(columns={'id': 'count'}).sort_values(['count'], ascending=False),
-    'full_name_count': df[['full_name', 'id']].groupby('full_name').count().rename(columns={'id': 'count'}).sort_values(['count'], ascending=False),
-    'service_group_count': df[['service_group', 'id']].groupby('service_group').count().rename(columns={'id': 'count'}).sort_values(['count'], ascending=False),
-    'priority_group_count': df[['priority_group', 'id']].groupby('priority_group').count().rename(columns={'id': 'count'}).sort_values(['count'], ascending=False),
-    'vaccine_date_count': df[['vaccine_date', 'id']].groupby('vaccine_date').count().rename(columns={'id': 'count'}).sort_values(['count'], ascending=False),
-}
+# DataFrame definitions
 
-query_results = {}
 
-for query_name, query in queries.items():
-    query.to_csv(''.join(['/'.join([output_path, query_name]), '.csv']), encoding='utf-8-sig')
+def vaccination_count_statistics():
+    df_ = pd.DataFrame(data={
+        'vaccinated': [len(df)],
+        'estimated_non_vaccinated': [MANAUS_ESTIMATED_POPULATION-len(df)],
+        'estimated_population': [MANAUS_ESTIMATED_POPULATION],
+    })
+
+    df_['estimated_vaccinated_percentage'] = df_['vaccinated']/df_['estimated_population']*100
+    df_['estimated_non_vaccinated_percentage'] = 100 - df_['estimated_vaccinated_percentage']
+
+    return df_
+
+
+def vaccination_site_count():
+    return df[['vaccination_site', 'id']]\
+        .groupby('vaccination_site')\
+        .count()\
+        .rename(columns={'id': 'count'})\
+        .sort_values(['count'], ascending=False)
+
+
+def area_count():
+    return df[['area', 'id']]\
+        .groupby('area')\
+        .count()\
+        .rename(columns={'id': 'count'})\
+        .sort_values(['count'], ascending=False)
+
+
+def cpf_count():
+    return df[['cpf', 'id']]\
+        .groupby('cpf')\
+        .count()\
+        .rename(columns={'id': 'count'})\
+        .sort_values(['count'], ascending=False)
+
+
+def full_name_count():
+    return df[['full_name', 'id']]\
+        .groupby('full_name')\
+        .count()\
+        .rename(columns={'id': 'count'})\
+        .sort_values(['count'], ascending=False)
+
+
+def service_group_count():
+    return df[['service_group', 'id']]\
+        .groupby('service_group')\
+        .count()\
+        .rename(columns={'id': 'count'})\
+        .sort_values(['count'], ascending=False)
+
+
+def priority_group_count():
+    return df[['priority_group', 'id']]\
+        .groupby('priority_group')\
+        .count()\
+        .rename(columns={'id': 'count'})\
+        .sort_values(['count'], ascending=False)
+
+
+def vaccine_date_count():
+    return df[['vaccine_date', 'id']]\
+        .groupby('vaccine_date')\
+        .count()\
+        .rename(columns={'id': 'count'})\
+        .sort_values(['vaccine_date'], ascending=True)
+
+
+def uncategorized_service_group_by_vaccination_site_count():
+    return df.loc[df['service_group'] == 'Outros', ['vaccination_site', 'id']]\
+        .groupby('vaccination_site')\
+        .count()\
+        .rename(columns={'id': 'count'})\
+        .sort_values(['count'], ascending=False)
+
+
+def uncategorized_service_group_by_vaccination_site_full_data():
+    df_ = uncategorized_service_group_by_vaccination_site_count()\
+        .rename(columns={'count': 'uncategorized_count'})\
+        .join(vaccination_site_count().rename(columns={'count': 'total_count'}))
+
+    df_['categorized_count'] = df_['total_count'] - df_['uncategorized_count']
+    df_['uncategorized_percent'] = df_['uncategorized_count']/df_['total_count']*100
+    df_['categorized_percent'] = df_['categorized_count']/df_['total_count']*100
+
+    return df_
+
+
+def uncategorized_service_group_by_vaccination_site_percent():
+    return uncategorized_service_group_by_vaccination_site_full_data()[['uncategorized_percent']]\
+        .sort_values(['uncategorized_percent'], ascending=False)
+
+
+dfs_to_extract = [
+    vaccination_count_statistics,
+    vaccination_site_count,
+    area_count,
+    cpf_count,
+    full_name_count,
+    service_group_count,
+    priority_group_count,
+    vaccine_date_count,
+    uncategorized_service_group_by_vaccination_site_full_data,
+    uncategorized_service_group_by_vaccination_site_percent
+]
+
+for df_to_extract in dfs_to_extract:
+    df_to_extract().to_csv(''.join(['/'.join([output_path, df_to_extract.__name__]), '.csv']), encoding='utf-8-sig')
